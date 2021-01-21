953fb06985ef35738b7b391330500a06e7d2b7b1
chris <chrislrogers94@gmail.com>
2021-01-21 14:28:45 +0000

    added charts that show the percentage of the 3 most used words

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const input = document.getElementById('inputfield');
 const counter = document.getElementById('counter');
 const unique = document.getElementById('unique');
 const results = document.getElementById("results");
+const top1 = document.getElementById("top1");
+const top2 = document.getElementById("top2");
+const top3 = document.getElementById("top3");
+const text1 = document.getElementById("text1");
+const text2 = document.getElementById("text2");
+const text3 = document.getElementById("text3");
+const title1 = document.getElementById("title1");
+const title2 = document.getElementById("title2");
+const title3 = document.getElementById("title3");
+
+const circumference = top1.r.baseVal.value * 2 * Math.PI;
+
+top1.style.strokeDasharray = circumference;
+top2.style.strokeDasharray = circumference;
+top3.style.strokeDasharray = circumference;
 
 input.addEventListener('input', function(e) { 
     update(e.target.value);
@@ -15,7 +30,11 @@ function update(input) {
     let sorted = [];
     
     counter.innerHTML = count(words);
-    unique.innerHTML = uniqueCount(words);
+    if (count(words) !== 0) {
+        unique.innerHTML = uniqueCount(words);
+    } else {
+        unique.innerHTML = '0';
+    }
     results.innerHTML = '';
     
     sorted = Object.entries(uniqueWords).sort((a, b) => b[1] - a[1]);
@@ -29,6 +48,33 @@ function update(input) {
                 <tr>
             `;
         }
+    }
+    if (sorted[0] !== undefined && (percent(count(words),sorted[0][1])) !== 0) {
+        draw(percent(count(words),sorted[0][1]), top1);
+        text1.innerHTML = percent(count(words),sorted[0][1]) + "%";
+        title1.innerHTML = sorted[0][0];
+    } else {
+        draw(0, top1);
+        text1.innerHTML = '';
+        title1.innerHTML = '';
+    }
+    if (sorted[1] !== undefined) {
+        draw(percent(count(words),sorted[1][1]), top2);
+        text2.innerHTML = percent(count(words),sorted[1][1]) + "%";
+        title2.innerHTML = sorted[1][0];
+    } else {
+        draw(0, top2);
+        text2.innerHTML = '';
+        title2.innerHTML = '';
+    }
+    if (sorted[2] !== undefined) {
+        draw(percent(count(words),sorted[2][1]), top3);
+        text3.innerHTML = percent(count(words),sorted[2][1]) + "%";
+        title3.innerHTML = sorted[2][0];
+    } else {
+        draw(0, top3);
+        text3.innerHTML = '';
+        title3.innerHTML = '';
     }
 }
 
@@ -48,6 +94,10 @@ function uniqueCount(input) {
     return num;
 }
 
+function draw(percentage, name) {
+    name.style.strokeDashoffset = circumference - (percentage / 100) * circumference;
+}
+
 function clean(input) {
     let str;
     str = input.replace(/[?.!,"\(\)<>]/g, "");
@@ -64,4 +114,13 @@ function count(input) {
         }
     }
     return num;
+}
+
+function percent(num, per) {
+    let sum = ((per/num) * 100);
+    if (sum === Infinity) {
+        return 0;
+    } else {
+    return sum.toFixed(1)
+    }
 }
